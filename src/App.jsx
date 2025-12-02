15f49787a1750ddfc8637282c5beaf2fdb066b2e
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2025-12-03 04:08:41 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Dashboard from "./Components/Dashboard/Dashboard";
 import Home from "./Components/Home/Home";
 import Register from "./Components/Register/Register";
 import { MyProvider } from "./Context/MyContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <MyProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </MyProvider>
   );
 }
