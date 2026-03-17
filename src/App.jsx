80d5e04abb85cff7fc90f983c5693c207d2e1dc7
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2026-03-18 03:50:02 +0530

    Add page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@
 import "./App.css";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Home from "./Components/Home/Home";
+import NotFound from "./Components/Home/NotFound";
 import Register from "./Components/Register/Register";
 import { MyProvider } from "./Context/MyContext";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <MyProvider>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </MyProvider>
   );
 }
